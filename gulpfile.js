e11beb0aec91bbed84d01a58e9872d6b15f88a9e
SvetlanaBakanova <69769350+SvetlanaBakanova@users.noreply.github.com>
2021-03-02 16:46:58 +0200

    новая сборка

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,45 @@
-const {
-    src,
-    dest,
-    watch,
-    parallel,
-    series
-} = require("gulp");
-const scss = require("gulp-sass");
-const prefix = require("gulp-autoprefixer");
-const sync = require("browser-sync").create();
-const imagemin = require("gulp-imagemin");
-const uglify = require("gulp-uglify");
+const { src, dest, watch, parallel, series} = require("gulp");
+const scss      = require("gulp-sass");
+const prefix    = require("gulp-autoprefixer");
+const sync      = require("browser-sync").create();
+const imagemin  = require("gulp-imagemin");
+// шрифты
+const ttf2woff  = require("gulp-ttf2woff");
+const ttf2woff2 = require("gulp-ttf2woff2");
 
+// создание файлов
+const fs        = require("fs");
+
+
+// files
+function createFiles() {
+    createFolders();
+    setTimeout(() => {
+        fs.writeFile("newfolder/index.html", "!", function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log("File created")
+        });
+        fs.writeFile("newfolder/scss/style.scss", "", function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log("File created")
+        })
+    }, 500);
+}
+
+// folders
+function createFolders() {
+    return src("*.*", { read: false })
+    .pipe(dest("./newfolder/scss/"))
+    .pipe(dest("./newfolder/js/"))
+    .pipe(dest("./newfolder/img/"))
+    .pipe(dest("./newfolder/fonts/"))
+}
+
+// dev
 function convertStyles() {
     return src('app/scss/style.scss')
         .pipe(scss({
@@ -23,12 +52,6 @@ function convertStyles() {
         }))
         .pipe(dest('app/css'))
 };
-
-function uglifyJS() {
-    return src('app/js/draft/*.js')
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-}
 
 function imageCompressed() {
     return src('app/_img/*.{jpg,png,svg}')
@@ -53,18 +76,21 @@ function watchFiles() {
 
     watch('app/js/*.js').on("change", sync.reload)
 
-    watch('app/js/draft/*.js', uglifyJS)
-
     watch('app/_img', imageCompressed)
+
+    // шрифты
+    watch("app/fonts/*.ttf", series(convertFonts, fontsStyle))
 }
 
-exports.convertStyles = convertStyles;
-exports.watchFiles = watchFiles;
-exports.browserSync = browserSync;
+exports.convertStyles   = convertStyles;
+exports.watchFiles      = watchFiles;
+exports.browserSync     = browserSync;
 exports.imageCompressed = imageCompressed;
-exports.uglifyJS = uglifyJS;
+//  struct - для создания папок и файлов
+exports.struct          = createFiles;
 
-exports.default = parallel(convertStyles, uglifyJS, browserSync, watchFiles);
+
+exports.default         = parallel(convertStyles, browserSync, watchFiles, series(convertFonts, fontsStyle));
 
 // Bulid
 function novehtml() {
@@ -87,8 +113,54 @@ function moveImgs() {
     .pipe(dest('dist/img'))
 }
 exports.novehtml = novehtml;
-exports.moveCss = moveCss;
-exports.moveJS = moveJS;
+exports.moveCss  = moveCss;
+exports.moveJS   = moveJS;
 exports.moveImgs = moveImgs;
 
-exports.bulid = series(novehtml, moveCss, moveJS, moveImgs);
+exports.bulid    = series(novehtml, moveCss, moveJS, moveImgs);
+
+// ф-ция для конвертации шрифтов
+function convertFonts() {
+    src(["app/fonts/**.ttf"])
+        .pipe(ttf2woff()).pipe(dest("app/fonts/"));
+    return src(["app/fonts/**.ttf"])
+        .pipe(ttf2woff2())
+        .pipe(dest("app/fonts/"));
+}
+
+// Конвертировать ttf шрифты
+exports.fontsStyle   = fontsStyle;
+exports.convertFonts = convertFonts;
+
+//! Font Face для шрифтов
+const cb = () => {};
+
+let srcFonts = "app/scss/_fonts.scss";
+let appFonts = "app/fonts";
+
+function fontsStyle() {
+    let file_content = fs.readFileSync(srcFonts);
+
+    fs.writeFile(srcFonts, "", cb);
+    fs.readdir(appFonts, function (err, items) {
+        if (items) {
+            let c_fontname;
+            for (let i = 0; i < items.length; i++) {
+                let fontname = items[i].split(".");
+                fontname = fontname[0];
+                if (c_fontname != fontname) {
+                    fs.appendFile(
+                        srcFonts,
+                        '@include font-face("' +
+                            fontname +
+                            '", "' +
+                            fontname +
+                            '", 400);',
+                        cb
+                    );
+                }
+                c_fontname = fontname;
+            }
+        }
+    });
+}
